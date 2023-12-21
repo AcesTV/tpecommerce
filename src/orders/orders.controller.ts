b7bb0c1fa69a5f9e2e4c93ff3dc6da9c4e3b2930
nbreant <nbreant@assit-evolution.com>
2023-12-21 12:49:34 +0100

    wip: orders

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { Order as OrderModel } from '@prisma/client';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Post()
+  create(
+    @Body() orderData: { userId: number; status: string; totalPrice: number },
+  ): Promise<OrderModel> {
+    return this.ordersService.create(orderData);
+  }
+
+  @Get()
+  findAll() {
+    return this.ordersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.ordersService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
+    return this.ordersService.update(+id, updateOrderDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.ordersService.remove(+id);
+  }
+}
